93a898662af5436ea188b34b98c7bf3812405d99
Ali Rido <rido.education@gmail.com>
2026-03-15 14:57:39 +0700

    implement the eslint + update invoice action

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,14 +13,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
-    params.set('page', '1')
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 500)
+  }, 500);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
